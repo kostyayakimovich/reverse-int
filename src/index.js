f60e0fc2c68c2a75a68649935016849634f1d1dd
kostyayakimovich <yakimovichkostya@gmail.com>
2020-03-07 16:23:53 +0300

    feat: solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    let res = String(n)
+        .split("")
+        .reverse()
+        .join("");
+    return Number(res);
+};
